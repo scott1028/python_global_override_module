c0e2cdbd2de48a53ad5853e9d9f52a8871bc7ebf
scott.lan <scott.lan@taisys.com>
2014-04-30 19:10:20 +0800

    up

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ from lib import echo
 
 echo()
 
-# 就可以 Override Global 的 Module Method
+# 就可以 Override Global 的 Module Method, 路徑要完整
+# 不能使用：
+# 	from lib_lib import do_in_echo
+#
+# 	do_in_echo = lambda: echo2()
+#
+# 	這樣他不會改到那個 Module 的物件。只會改道 Local Function Ref, 只有在這個 Main 有效。
+
 lib_lib.do_in_echo = lambda: echo2()
 
 from lib import echo
